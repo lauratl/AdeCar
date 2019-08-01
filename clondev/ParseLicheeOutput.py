7b904833c66926cc530436fb73a9dead5dbc0c90
Laura Tomas <lauratomaslopez@gmail.com>
2019-08-01 20:42:08 +0200

    Fixed topology interpretation

--- clondev/ParseLicheeOutput.py
+++ clondev/ParseLicheeOutput.py
@@ -10,7 +10,7 @@ output = open(argv[2], 'w')
 nodes=False
 tree=False
 snvinfosection=False
-
+clonesToClusters=False
 snvsInNodes = {}
 snvsInfo = {}
 
@@ -19,35 +19,75 @@ for linea in lichee:
         nodes=False
     elif linea[:12]=="Error score:":
         tree=False 
+	
         
     elif(nodes):
+	
         linea=linea[:-1]
         linea = linea.split("]")
         nodeNumber = linea[0].split("\t")[0]
         snvIds = linea[1].split("\t")[1:]
-        snvsInNodes[nodeNumber] = snvIds
+	snvNumbers = []
+	for snvId in snvIds:
+		snvNumbers.append(int(snvId[3:]))
+      				
+        snvsInNodes[nodeNumber] = snvNumbers
         
     elif(tree):
         linea = linea[:-1].split(" -> ")
-        if linea[0]=="0":
-            continue
-        snvsInNodes[linea[1]]+=snvsInNodes[linea[0]]
+        #if linea[0]=="0":
+        #    continue
+        topology_pairs.append([linea[0],linea[1]])
     
     elif(snvinfosection):
         linea= linea[:-1].split(" ")
-        snvId = linea[0][:-1]
+        snvId = int(linea[0][:-1][3:])
         snvsInfo[snvId] = linea[1:]
+    
+    elif(clonesToClusters):
+	
+	new_origins = ["0"]
+        while True:
+            origins = new_origins
+	    new_origins = []
+	    for origin in origins:
+
+	        for topology_pair in topology_pairs:
+ 	            if topology_pair[0]==origin:
+			
+		
+		        if origin != "0":
+			    snvsInNodes[topology_pair[1]]+=snvsInNodes[topology_pair[0]]
+                        new_origins.append(topology_pair[1])
+	    if len(new_origins) == 0:
+                break
+
         
-        
+
+
+	clonesToClusters = False 
         
     if linea[:5]=="Nodes":
         nodes=True
+
+
+    elif linea[:12]=="Error score:":
+	clonesToClusters=True
+
     elif linea[:14]=="****Tree 0****":
         tree=True
         nodes=False
+
+	topology = {}
+	for node in snvsInNodes.keys():
+		topology[node] = [node]
+
+	topology_pairs = []
     elif linea[:9]=="SNV info:":
         snvinfosection = True
-        
+
+
+
     
 output.write(">healthy\n")
 
@@ -58,8 +98,8 @@ output.write("\n")
 
 for clon in sorted(snvsInNodes.keys()):
     output.write(">clon_"+str(clon)+"\n")
-    for snv in snvsInfo.keys():
-        if snv in snvsInNodes[clon]:
+    for snv in sorted(snvsInfo.keys()):
+	if snv in snvsInNodes[clon]:
             output.write(snvsInfo[snv][2].split("/")[1])
         else:
             output.write(snvsInfo[snv][2].split("/")[0])
